6dfd2caadce92ea49981354254c1f21a800c70e9
Dzhavat Ushev <dzhavatushev@gmail.com>
2022-09-16 07:00:03 +0200

    chore: format file

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,9 @@
-import * as vscode from 'vscode';
-import { tsquery } from '@phenomnomnominal/tsquery';
-import { Node } from 'typescript';
+import * as vscode from "vscode";
+import { tsquery } from "@phenomnomnominal/tsquery";
+import { Node } from "typescript";
 
-const FDESCRIBE_FIT_QUERY = 'CallExpression > Identifier[name=/^f(describe|it)$/]';
+const FDESCRIBE_FIT_QUERY =
+	"CallExpression > Identifier[name=/^f(describe|it)$/]";
 const ONLY_QUERY = 'PropertyAccessExpression > Identifier[name="only"]';
 
 let textEditorDecoration: vscode.TextEditorDecorationType;
@@ -12,20 +13,28 @@ export function activate(context: vscode.ExtensionContext): void {
 
 	textEditorDecoration = createTextEditorDecoration(context);
 
-	vscode.window.onDidChangeActiveTextEditor(editor => {
-		activeEditor = editor;
+	vscode.window.onDidChangeActiveTextEditor(
+		(editor) => {
+			activeEditor = editor;
 
-		if (editor) {
-			decorate(editor);
-		}
-	}, null, context.subscriptions);
+			if (editor) {
+				decorate(editor);
+			}
+		},
+		null,
+		context.subscriptions
+	);
 
-	vscode.workspace.onDidChangeTextDocument(event => {
-		if (activeEditor && activeEditor.document === event.document) {
-			decorate(activeEditor);
-		}
-	}, null, context.subscriptions);
-	
+	vscode.workspace.onDidChangeTextDocument(
+		(event) => {
+			if (activeEditor && activeEditor.document === event.document) {
+				decorate(activeEditor);
+			}
+		},
+		null,
+		context.subscriptions
+	);
+
 	if (activeEditor) {
 		decorate(activeEditor);
 	}
@@ -34,9 +43,9 @@ export function activate(context: vscode.ExtensionContext): void {
 function createTextEditorDecoration(context: vscode.ExtensionContext) {
 	return vscode.window.createTextEditorDecorationType({
 		overviewRulerLane: 4,
-		gutterIconPath: context.asAbsolutePath('/images/warning.svg'),
-		overviewRulerColor: new vscode.ThemeColor('editorWarning.foreground'),
-		gutterIconSize: '1rem',
+		gutterIconPath: context.asAbsolutePath("/images/warning.svg"),
+		overviewRulerColor: new vscode.ThemeColor("editorWarning.foreground"),
+		gutterIconSize: "1rem",
 	});
 }
 
@@ -48,14 +57,20 @@ function decorate(activeEditor: vscode.TextEditor) {
 	const sourceCode = activeEditor.document.getText();
 	let decorationOptions: vscode.DecorationOptions[] = [];
 
-	tsquery(sourceCode, FDESCRIBE_FIT_QUERY).map(result => {
-		const decoration = createDecorationOption(activeEditor, result.parent.parent);
+	tsquery(sourceCode, FDESCRIBE_FIT_QUERY).map((result) => {
+		const decoration = createDecorationOption(
+			activeEditor,
+			result.parent.parent
+		);
 
 		decorationOptions.push(decoration);
 	});
 
-	tsquery(sourceCode, ONLY_QUERY).map(result => {
-		const decoration = createDecorationOption(activeEditor, result.parent.parent.parent);
+	tsquery(sourceCode, ONLY_QUERY).map((result) => {
+		const decoration = createDecorationOption(
+			activeEditor,
+			result.parent.parent.parent
+		);
 
 		decorationOptions.push(decoration);
 	});
@@ -66,7 +81,7 @@ function decorate(activeEditor: vscode.TextEditor) {
 function isSpecFile(document: vscode.TextDocument): boolean {
 	const fileNameEndsWith = /[-|.]spec\.[j|t]s$/;
 
-	return !document.isUntitled && (fileNameEndsWith.test(document.fileName));
+	return !document.isUntitled && fileNameEndsWith.test(document.fileName);
 }
 
 function createDecorationOption(activeEditor: vscode.TextEditor, node: Node) {
@@ -75,7 +90,10 @@ function createDecorationOption(activeEditor: vscode.TextEditor, node: Node) {
 
 	const decoration: vscode.DecorationOptions = {
 		range: new vscode.Range(startPosition, endPosition),
-		hoverMessage: new vscode.MarkdownString('$(warning) Focused test. Be careful not to commit it!', true),
+		hoverMessage: new vscode.MarkdownString(
+			"$(warning) Focused test. Be careful not to commit it!",
+			true
+		),
 	};
 
 	return decoration;
